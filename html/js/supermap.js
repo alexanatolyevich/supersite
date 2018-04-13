cc0052fda2738eadee59d82c9208d84d5002afdc
alexanatolyevich <bezrukaleksandr11@gmail.com>
2018-04-13 17:49:51 +0300

    map marker

--- html/js/supermap.js
+++ html/js/supermap.js
@@ -1,14 +1,48 @@
+let map = {};
+let markerCoords = {};
+let marker = {};
 function initMap() {
 	const centerLatLng = new google.maps.LatLng(50.466554, 30.476847);
 	const mapOption = {
 		center: centerLatLng,
 		zoom: 8
 	};
-	const map = new google.maps.Map(document.getElementById('map'), mapOption);
+	map = new google.maps.Map(document.getElementById('map'), mapOption);
+	
 
 }
+function initialize(){
+	let input = document.getElementById('autocomplete');
+	let options = {componentRestrictions: {country: 'ua'}};
+
+	const autocomplete = new google.maps.places.Autocomplete(input, options);
+	google.maps.event.addListener(autocomplete, 'place_changed', function() {
+		let place = autocomplete.getPlace();
+		//console.log(place);
+		const lat = place.geometry.location.lat();
+		const lng = place.geometry.location.lng();
+
+		markerCoords = new google.maps.LatLng(lat, lng);
+
+	});
+}
+
 
 $(document).ready(() => {
 
 	initMap();
+	initialize();
+
+	$('.show').on('click', (event) => {
+		event.preventDefault();
+		if(!$.isEmptyObject(marker)){
+			marker.setMap(null);
+		}
+		marker = new google.maps.Marker({
+			position: markerCoords,
+			map: map
+		});
+		map.setCenter(markerCoords);
+		//marker.setMap(map);
+	});
 });
